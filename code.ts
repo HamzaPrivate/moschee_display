9a898184d316aa1fa322b5f250bacc9165327c1e
HamzaPrivate <122999590+HamzaPrivate@users.noreply.github.com>
2023-04-10 05:33:52 +0200

    Add files via upload

--- code.ts
+++ code.ts
@@ -6,7 +6,7 @@ console.log("1. now and midnight and timeUntilMidnight",now, midnight, timeUntil
 var infosImg = document.getElementById("infos")! as HTMLImageElement;
 var ahadithImg = document.getElementById("ahadith")! as HTMLImageElement;
 var infosSources = ["infos/ramazan0.jpeg", "infos/info0.jpeg", "infos/ramazan1.jpeg"];
-var ahadithSources = ["ahadith/ramazan0.jpeg", "ahadith/ramazan1.jpeg"];
+var ahadithSources = ["ahadith/ramazan0.jpeg", "ahadith/ramazan1.jpeg", "ahadith/ramazan2.jpeg"];
 var infosIndex = Math.floor(Math.random() * infosSources.length);
 var ahadithIndex = Math.floor(Math.random() * ahadithSources.length);
 infosImg.src = infosSources[infosIndex];
@@ -17,17 +17,6 @@ document.querySelector("body")?.addEventListener("click", () => {
 })
 
 const map = new Map<string, string>();
-map.set("Mar 30 2023", "5:07|6:45|13:11|16:41|19:37|21:16");
-map.set("Mar 31 2023", "5:04|6:43|13:11|16:42|19:39|21:18");
-map.set("Apr 01 2023", "5:01|6:41|13:11|16:43|19:41|21:20");
-map.set("Apr 02 2023", "4:58|6:38|13:11|16:44|19:43|21:23");
-map.set("Apr 03 2023", "4:56|6:36|13:10|16:45|19:44|21:25");
-map.set("Apr 04 2023", "4:53|6:34|13:10|16:46|19:45|21:27");
-map.set("Apr 05 2023", "4:50|6:31|13:10|16:47|19:48|21:29");
-map.set("Apr 06 2023", "4:47|6:29|13:09|16:48|19:50|21:31");
-map.set("Apr 07 2023", "4:44|6:27|13:09|16:49|19:51|21:34");
-map.set("Apr 08 2023", "4:42|6:25|13:09|16:50|19:53|21:36");
-map.set("Apr 09 2023", "4:39|6:22|13:09|16:51|19:55|21:38");
 map.set("Apr 10 2023", "4:36|6:20|13:08|16:51|19:57|21:41");
 map.set("Apr 11 2023", "4:33|6:18|13:08|16:52|19:58|21:43");
 map.set("Apr 12 2023", "4:30|6:15|13:08|16:53|20:00|21:45");
@@ -39,6 +28,16 @@ map.set("Apr 17 2023", "4:15|6:04|13:06|16:57|20:09|21:58");
 map.set("Apr 18 2023", "4:12|6:02|13:06|16:58|20:11|22:00");
 map.set("Apr 19 2023", "4:09|6:00|13:06|16:59|20:12|22:03");
 map.set("Apr 20 2023", "4:06|5:58|13:06|17:00|20:14|22:05");
+map.set("Apr 21 2023", "4:03|5:56|13:06|17:00|20:16|22:08");
+map.set("Apr 22 2023", "4:00|5:53|13:05|17:01|20:18|22:10");
+map.set("Apr 23 2023", "3:57|5:51|13:05|17:02|20:19|22:13");
+map.set("Apr 24 2023", "3:54|5:49|13:05|17:03|20:21|22:16");
+map.set("Apr 25 2023", "3:51|5:47|13:05|17:03|20:23|22:18");
+map.set("Apr 26 2023", "3:48|5:45|13:05|17:04|20:25|22:21");
+map.set("Apr 27 2023", "3:45|5:43|13:05|17:05|20:26|22:24");
+map.set("Apr 28 2023", "3:42|5:41|13:04|17:06|20:28|22:27");
+map.set("Apr 29 2023", "3:39|5:39|13:04|17:06|20:30|22:30");
+map.set("Apr 30 2023", "3:36|5:37|13:04|17:07|20:31|22:32");
 
 
 const prayerTable = document.getElementById("zeiten") as HTMLTableElement;
