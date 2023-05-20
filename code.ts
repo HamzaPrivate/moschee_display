79c53bff0f40768d72b371447c7b4a7173590333
Hamza Hodzic <s88339@bht-berlin.de>
2023-05-20 13:36:07 +0200

    clock adjustments

--- code.ts
+++ code.ts
@@ -3,24 +3,6 @@ document.querySelector("body")?.addEventListener("click", () => {
 })
 const map = new Map<string, string>();
 
-map.set("May 01 2023", "3:33|5:35|13:04|17:08|20:33|22:35");
-map.set("May 02 2023", "3:27|5:33|13:04|17:09|20:35|22:40");
-map.set("May 03 2023", "3:26|5:31|13:04|17:09|20:37|22:41");
-map.set("May 04 2023", "3:25|5:29|13:04|17:10|20:38|22:42");
-map.set("May 05 2023", "3:24|5:27|13:04|17:11|20:40|22:43");
-map.set("May 06 2023", "3:23|5:25|13:04|17:11|20:42|22:44");
-map.set("May 07 2023", "3:22|5:24|13:03|17:12|20:43|22:45");
-map.set("May 08 2023", "3:21|5:22|13:03|17:13|20:45|22:45");
-map.set("May 09 2023", "3:20|5:20|13:03|17:13|20:47|22:46");
-map.set("May 10 2023", "3:19|5:18|13:03|17:14|20:48|22:47");
-map.set("May 11 2023", "3:18|5:17|13:03|17:15|20:50|22:48");
-map.set("May 12 2023", "3:17|5:15|13:03|17:15|20:52|22:49");
-map.set("May 13 2023", "3:16|5:13|13:03|17:16|20:53|22:50");
-map.set("May 14 2023", "3:15|5:12|13:03|17:16|20:55|22:50");
-map.set("May 15 2023", "3:14|5:10|13:03|17:17|20:56|22:51");
-map.set("May 16 2023", "3:13|5:09|13:03|17:18|20:58|22:52");
-map.set("May 17 2023", "3:13|5:07|13:03|17:18|20:59|22:53");
-map.set("May 18 2023", "3:12|5:06|13:03|17:19|21:01|22:54");
 map.set("May 19 2023", "3:11|5:04|13:03|17:20|21:03|22:55");
 map.set("May 20 2023", "3:10|5:03|13:03|17:20|21:04|22:55");
 map.set("May 21 2023", "3:10|5:01|13:03|17:21|21:05|22:56");
@@ -35,32 +17,27 @@ map.set("May 29 2023", "3:05|4:52|13:04|17:25|21:16|23:02");
 map.set("May 30 2023", "3:05|4:51|13:04|17:26|21:18|23:03");
 map.set("May 31 2023", "3:04|4:50|13:05|17:26|21:19|23:04");
 
-map.set("Apr 30 2023", "3:36|5:37|13:04|17:07|20:31|22:32");
-//
+
 var now = new Date();
 var midnight = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
 var timeUntilMidnight = midnight.getTime() - now.getTime();
 console.log("1. now and midnight and timeUntilMidnight", now, midnight, timeUntilMidnight);
-//
+
 var broadImg = document.getElementById("broad") as HTMLImageElement;
 var infosImg = document.getElementById("infos")! as HTMLImageElement;
 var ahadithImg = document.getElementById("ahadith")! as HTMLImageElement;
 var video = document.getElementById("vid")! as HTMLVideoElement;
-//
-var infosSources = ["infos/info0.jpeg"];//video sources used to be here
-var ahadithSources = ["ahadith/a0.jpeg"];
-var broadSources = ["broad/broad1.jpeg", "broad/sufara.jpeg", "broad/halka.jpeg"];
-//
+
+var infosSources = ["infos/i0.jpeg", "infos/i1.jpeg", "infos/i2.jpeg"];//upright video sources possible
+var ahadithSources = ["ahadith/a0.jpeg", "ahadith/a1.jpeg"];
+var broadSources = ["broad/broad1.jpeg", "broad/sufara.jpeg", "broad/halka.jpeg"];//broad video sources possible
+
 var infosIndex = Math.floor(Math.random() * infosSources.length);
 var ahadithIndex = Math.floor(Math.random() * ahadithSources.length);
 var broadIndex = Math.floor(Math.random() * broadSources.length);
 
 if (videoComing()) displayVideo();
-else {
-    // infosImg.src = infosSources[infosIndex];
-    ahadithImg.src = ahadithSources[ahadithIndex];
-    video.style.display = "none";
-}
+else video.style.display = "none";
 
 const prayerTable = document.getElementById("zeiten") as HTMLTableElement;
 const prayerTimes = map.get(getFormattedDate("" + now).trim());//check
@@ -103,7 +80,7 @@ function getFormattedDate(date: string): string {
 /**
  * 
  * @param indexToExclude current index to the current picture
- * @param pictureGroup the respective array where the files(paths) are stored in
+ * @param pictureGroup the respective array where the files are stored in
  * @returns 
  */
 function getNewPic(indexToExclude: number, pictureGroup: string[]): number {
