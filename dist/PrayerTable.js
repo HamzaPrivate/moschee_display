b3fe27b5e88d11a735bebb0b516e19853ae2b729
Hamza Hodzic <s88339@bht-berlin.de>
2023-12-03 19:31:38 +0100

    removed islamic midnight

--- dist/PrayerTable.js
+++ dist/PrayerTable.js
@@ -17,7 +17,7 @@ var midnight = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
 exports.timeUntilMidnight = midnight.getTime() - now.getTime();
 const prayerTable = document.getElementsByClassName("namaztime");
 var textCol = prayerTable[0].style.color;
-var islMitternacht = document.getElementById("islamische-mitternacht");
+//var islMitternacht = document.getElementById("islamische-mitternacht")!;
 /**
 * Initializes the prayer table by injecting the corresponding prayer times for the current date.
 * If the prayer table and prayer times are available, the function populates the table cells with the times.
@@ -28,40 +28,40 @@ function initiatePrayerTable() {
         const prayerTimes = yield (0, PrayerTimes_1.getTodaysPrayerTimes)();
         //injecting prayer times into the table
         // console.log(prayerTimes);
-        islMitternacht.textContent = calculateMiddleTime(prayerTimes[0], prayerTimes[5]);
+        //islMitternacht.textContent = calculateMiddleTime(prayerTimes[0], prayerTimes[5]);
         if (prayerTable && prayerTimes) {
             for (let i = 0; i < prayerTimes.length; i++) {
                 prayerTable[i].textContent = prayerTimes[i];
             }
-            islMitternacht.textContent = calculateMiddleTime(prayerTimes[4], prayerTimes[0]);
+            //islMitternacht.textContent = calculateMiddleTime(prayerTimes[4], prayerTimes[0]);
         }
         else
             throw new Error(`Fehler in der Gebetszeitentabelle. prayerTable:${prayerTable}, prayerTimes: ${prayerTimes}`);
     });
 }
 exports.initiatePrayerTable = initiatePrayerTable;
-function calculateMiddleTime(time1, time2) {
-    // Create a base date for calculations (ignoring the year, month, and day)
-    const baseDate = new Date(2000, 0, 1);
-    // Parse the time strings and set them on the base date
-    const [hours1, minutes1] = time1.split(':');
-    const [hours2, minutes2] = time2.split(':');
-    let date1 = new Date(baseDate);
-    date1.setHours(Number(hours1), Number(minutes1), 0);
-    let date2 = new Date(baseDate);
-    date2.setHours(Number(hours2), Number(minutes2), 0);
-    // Check if the second time is earlier than the first one
-    if (date2 < date1) {
-        date2.setDate(date2.getDate() + 1); // Add one day to the second time
-    }
-    // Calculate the average time in milliseconds
-    const averageTimeInMilliseconds = (date1.getTime() + date2.getTime()) / 2;
-    // Create a new Date object with the average time
-    const middleTime = new Date(averageTimeInMilliseconds);
-    // Format the result as a time string (HH:mm)
-    const formattedMiddleTime = middleTime.toTimeString().slice(0, 5);
-    return formattedMiddleTime;
-}
+//function calculateMiddleTime(time1: string, time2: string) {
+//     // Create a base date for calculations (ignoring the year, month, and day)
+//     const baseDate = new Date(2000, 0, 1);
+//     // Parse the time strings and set them on the base date
+//     const [hours1, minutes1] = time1.split(':');
+//     const [hours2, minutes2] = time2.split(':');
+//     let date1 = new Date(baseDate);
+//     date1.setHours(Number(hours1), Number(minutes1), 0);
+//     let date2 = new Date(baseDate);
+//     date2.setHours(Number(hours2), Number(minutes2), 0);
+//     // Check if the second time is earlier than the first one
+//     if (date2 < date1) {
+//         date2.setDate(date2.getDate() + 1); // Add one day to the second time
+//     }
+//     // Calculate the average time in milliseconds
+//     const averageTimeInMilliseconds = (date1.getTime() + date2.getTime()) / 2;
+//     // Create a new Date object with the average time
+//     const middleTime = new Date(averageTimeInMilliseconds);
+//     // Format the result as a time string (HH:mm)
+//     const formattedMiddleTime = middleTime.toTimeString().slice(0, 5);
+//     return formattedMiddleTime;
+// }
 // // Example usage:
 // const time1 = "15:57";
 // const time2 = "6:11";
