0c67c9ff7fa56dcff28288ac29015cd9af2403f1
Hamza Hodzic <s88339@bht-berlin.de>
2024-04-10 16:32:18 +0200

    mess

--- dist/PrayerTable.js
+++ dist/PrayerTable.js
@@ -16,7 +16,9 @@ var now = new Date();
 var midnight = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
 exports.timeUntilMidnight = midnight.getTime() - now.getTime();
 const prayerTable = document.getElementsByClassName("namaztime");
+var fajr, ishraq, dhuhr, asr, maghrib, isha;
 var textCol = prayerTable[0].style.color;
+var isPrayerPinpointed = false;
 //var islMitternacht = document.getElementById("islamische-mitternacht")!;
 /**
 * Initializes the prayer table by injecting the corresponding prayer times for the current date.
@@ -26,6 +28,7 @@ var textCol = prayerTable[0].style.color;
 function initiatePrayerTable() {
     return __awaiter(this, void 0, void 0, function* () {
         const prayerTimes = yield (0, PrayerTimes_1.getTodaysPrayerTimes)();
+        savePrayerTimes(prayerTimes);
         //injecting prayer times into the table
         // console.log(prayerTimes);
         //islMitternacht.textContent = calculateMiddleTime(prayerTimes[0], prayerTimes[5]);
@@ -40,33 +43,6 @@ function initiatePrayerTable() {
     });
 }
 exports.initiatePrayerTable = initiatePrayerTable;
-//function calculateMiddleTime(time1: string, time2: string) {
-//     // Create a base date for calculations (ignoring the year, month, and day)
-//     const baseDate = new Date(2000, 0, 1);
-//     // Parse the time strings and set them on the base date
-//     const [hours1, minutes1] = time1.split(':');
-//     const [hours2, minutes2] = time2.split(':');
-//     let date1 = new Date(baseDate);
-//     date1.setHours(Number(hours1), Number(minutes1), 0);
-//     let date2 = new Date(baseDate);
-//     date2.setHours(Number(hours2), Number(minutes2), 0);
-//     // Check if the second time is earlier than the first one
-//     if (date2 < date1) {
-//         date2.setDate(date2.getDate() + 1); // Add one day to the second time
-//     }
-//     // Calculate the average time in milliseconds
-//     const averageTimeInMilliseconds = (date1.getTime() + date2.getTime()) / 2;
-//     // Create a new Date object with the average time
-//     const middleTime = new Date(averageTimeInMilliseconds);
-//     // Format the result as a time string (HH:mm)
-//     const formattedMiddleTime = middleTime.toTimeString().slice(0, 5);
-//     return formattedMiddleTime;
-// }
-// // Example usage:
-// const time1 = "15:57";
-// const time2 = "6:11";
-// const middleTime = calculateMiddleTime(time1, time2);
-// console.log("Middle Time:", middleTime);
 /**
 * Calculates the time remaining until the next prayer based on the current time and the prayer table.
 * The function determines the closest prayer time from the table and sets it as the target time.
@@ -118,16 +94,50 @@ function calcTimeTillPrayer() {
 exports.calcTimeTillPrayer = calcTimeTillPrayer;
 function adaptBackground() {
     const bg = document.querySelector("body");
-    const time = new Date();
-    const hour = time.getHours();
-    if (hour >= 6 && hour < 14) {
+    const date = new Date();
+    const time = "" + date.getHours() + "" + date.getMinutes();
+    const hours = date.getHours();
+    if (time === fajr) {
+        bg.style.backgroundImage = "url('pictures/broad/dark.webp')";
+        isPrayerPinpointed = true;
+    }
+    else if (time === ishraq) {
         bg.style.backgroundImage = "url('pictures/broad/early.webp')";
+        isPrayerPinpointed = true;
     }
-    else if (hour >= 12 && hour < 18) {
-        bg.style.backgroundImage = "url('pictures/broad/late.webp')";
+    else if (time === dhuhr || time === asr) {
+        bg.style.backgroundImage = "url('pictures/broad/mid.webp')";
+        isPrayerPinpointed = true;
     }
-    else if ((hour >= 18 && hour < 24) || (hour >= 0 && hour < 6)) {
+    else if (time === maghrib) {
         bg.style.backgroundImage = "url('pictures/broad/late.webp')";
+        isPrayerPinpointed = true;
+    }
+    else if (time === isha) {
+        bg.style.backgroundImage = "url('pictures/broad/night.webp')";
+        isPrayerPinpointed = true;
+    }
+    else if (!isPrayerPinpointed) {
+        if (hours >= 0 && hours < 5) {
+            bg.style.backgroundImage = "url('pictures/broad/dark.webp')";
+        }
+        else if (hours >= 5 && hours < 8) {
+            bg.style.backgroundImage = "url('pictures/broad/early.webp')";
+        }
+        else if (hours >= 8 && hours < 18) {
+            bg.style.backgroundImage = "url('pictures/broad/mid.webp')";
+        }
+        else if (hours >= 18 && hours < 24) {
+            bg.style.backgroundImage = "url('pictures/broad/late.webp')";
+        }
     }
 }
 exports.adaptBackground = adaptBackground;
+function savePrayerTimes(prayerTimes) {
+    fajr = prayerTimes[0];
+    ishraq = prayerTimes[1];
+    dhuhr = prayerTimes[2];
+    asr = prayerTimes[3];
+    maghrib = prayerTimes[4];
+    isha = prayerTimes[5];
+}
--- src/PrayerTable.ts
+++ src/PrayerTable.ts
@@ -6,7 +6,9 @@ var now = new Date();
 var midnight = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
 export const timeUntilMidnight = midnight.getTime() - now.getTime();
 const prayerTable = document.getElementsByClassName("namaztime") as HTMLCollectionOf<HTMLSpanElement>;
+var fajr: string, ishraq: string, dhuhr: string, asr: string, maghrib: string, isha: string;
 var textCol = prayerTable[0].style.color;
+var isPrayerPinpointed = false;
 //var islMitternacht = document.getElementById("islamische-mitternacht")!;
 
 
@@ -17,6 +19,7 @@ var textCol = prayerTable[0].style.color;
 */
 export async function initiatePrayerTable() {
     const prayerTimes = await getTodaysPrayerTimes();
+    savePrayerTimes(prayerTimes);
     //injecting prayer times into the table
     // console.log(prayerTimes);
     //islMitternacht.textContent = calculateMiddleTime(prayerTimes[0], prayerTimes[5]);
@@ -30,46 +33,6 @@ export async function initiatePrayerTable() {
 
     else throw new Error(`Fehler in der Gebetszeitentabelle. prayerTable:${prayerTable}, prayerTimes: ${prayerTimes}`);
 }
-
-//function calculateMiddleTime(time1: string, time2: string) {
-//     // Create a base date for calculations (ignoring the year, month, and day)
-//     const baseDate = new Date(2000, 0, 1);
-
-//     // Parse the time strings and set them on the base date
-//     const [hours1, minutes1] = time1.split(':');
-//     const [hours2, minutes2] = time2.split(':');
-
-//     let date1 = new Date(baseDate);
-//     date1.setHours(Number(hours1), Number(minutes1), 0);
-
-//     let date2 = new Date(baseDate);
-//     date2.setHours(Number(hours2), Number(minutes2), 0);
-
-//     // Check if the second time is earlier than the first one
-//     if (date2 < date1) {
-//         date2.setDate(date2.getDate() + 1); // Add one day to the second time
-//     }
-
-//     // Calculate the average time in milliseconds
-//     const averageTimeInMilliseconds = (date1.getTime() + date2.getTime()) / 2;
-
-//     // Create a new Date object with the average time
-//     const middleTime = new Date(averageTimeInMilliseconds);
-
-//     // Format the result as a time string (HH:mm)
-//     const formattedMiddleTime = middleTime.toTimeString().slice(0, 5);
-
-//     return formattedMiddleTime;
-// }
-
-// // Example usage:
-// const time1 = "15:57";
-// const time2 = "6:11";
-
-// const middleTime = calculateMiddleTime(time1, time2);
-
-// console.log("Middle Time:", middleTime);
-
 
 /**
 * Calculates the time remaining until the next prayer based on the current time and the prayer table.
@@ -121,15 +84,49 @@ export function calcTimeTillPrayer() {
 
 export function adaptBackground() {
     const bg = document.querySelector("body")!;
-    const time = new Date();
-    const hour = time.getHours();
-    if(hour >= 6 && hour < 14) {
+    const date = new Date();
+    const time = "" + date.getHours()+ "" + date.getMinutes();
+    const hours = date.getHours();
+    if(time === fajr) {
+        bg.style.backgroundImage = "url('pictures/broad/dark.webp')";
+        isPrayerPinpointed = true;
+    }
+    else if(time === ishraq) {
         bg.style.backgroundImage = "url('pictures/broad/early.webp')";
+        isPrayerPinpointed = true;
     }
-    else if(hour >= 12 && hour < 18) {
+    else if(time === dhuhr || time === asr) {
+        bg.style.backgroundImage = "url('pictures/broad/mid.webp')";
+        isPrayerPinpointed = true;
+    }
+    else if(time === maghrib) {
         bg.style.backgroundImage = "url('pictures/broad/late.webp')";
+        isPrayerPinpointed = true;
     }
-    else if((hour >= 18 && hour < 24) || (hour >= 0 && hour < 6)) {
-        bg.style.backgroundImage = "url('pictures/broad/late.webp')";
+    else if(time === isha) {
+        bg.style.backgroundImage = "url('pictures/broad/night.webp')";
+        isPrayerPinpointed = true;
+    } else if(!isPrayerPinpointed) {
+        if(hours >= 0 && hours < 5) {
+            bg.style.backgroundImage = "url('pictures/broad/dark.webp')";
+        }
+        else if(hours >= 5 && hours < 8) {
+            bg.style.backgroundImage = "url('pictures/broad/early.webp')";
+        }
+        else if(hours >= 8 && hours < 18) {
+            bg.style.backgroundImage = "url('pictures/broad/mid.webp')";
+        }
+        else if(hours >= 18 && hours < 24) {
+            bg.style.backgroundImage = "url('pictures/broad/late.webp')";
+        }
     }
+}
+
+function savePrayerTimes(prayerTimes: string[]){
+    fajr = prayerTimes[0];
+    ishraq = prayerTimes[1];
+    dhuhr = prayerTimes[2];
+    asr = prayerTimes[3];
+    maghrib = prayerTimes[4];
+    isha = prayerTimes[5];
 }
