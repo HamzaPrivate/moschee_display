2011b7369e9e73ca17da9338e907d509d93046dc
Hamza Hodzic <s88339@bht-berlin.de>
2025-02-23 23:12:20 +0100

    dsa

--- src/ResourceDisplayer.ts
+++ src/ResourceDisplayer.ts
@@ -6,7 +6,7 @@ var video = document.getElementById("vid") as HTMLVideoElement;
 
 const path = "pictures/narrow/";
 var narrow1Sources = [`${path}n8.jpeg`,`${path}n6.jpeg`,  `${path}n5.jpeg`, `${path}n3.jpeg`];
-var narrow2Sources = [ `${path}n1.jpeg`, `${path}halka.jpeg`,`${path}n2.jpeg`,`${path}7.jpeg`];
+var narrow2Sources = [ `${path}n1.jpeg`, `${path}halka.jpeg`,`${path}n2.jpeg`];
 var broadSources = [`pictures/broad/b0.jpeg`];//broad video sources possible
 
 //2 narrow pictures fit on the display
