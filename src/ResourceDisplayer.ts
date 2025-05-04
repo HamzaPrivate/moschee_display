baa815771571d252b5ea5ef7e96e4c1514052ca0
Hamza Hodzic <s88339@bht-berlin.de>
2025-05-04 11:04:05 +0200

    fds

--- src/ResourceDisplayer.ts
+++ src/ResourceDisplayer.ts
@@ -5,8 +5,8 @@ var narrow2 = document.getElementById("narrow2") as HTMLImageElement;
 var video = document.getElementById("vid") as HTMLVideoElement;
 
 const path = "pictures/narrow/";
-var narrow1Sources = [`${path}n1.jpeg`,`${path}n2.jpeg`,`${path}n3.jpeg`];
-var narrow2Sources = [ `${path}n4.jpeg`,`${path}n5.jpeg`, `${path}n6.jpeg`];
+var narrow1Sources = [`${path}n1.jpeg`,`${path}n2.jpeg`,`${path}n3.jpeg`, `${path}n8.jpeg`, `${path}n9.jpeg`];
+var narrow2Sources = [ `${path}n4.jpeg`,`${path}n5.jpeg`, `${path}n6.jpeg`, `${path}n7.jpeg`];
 var broadSources = [`pictures/broad/b0.jpeg`];//broad video sources possible
 
 //2 narrow pictures fit on the display
